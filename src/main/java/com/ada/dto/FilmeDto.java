6e1d6078d6c623a801a59bb46cbaa8467b1b51c6
André Igor <andreigormenezes@gmail.com>
2022-10-20 00:48:45 -0300

    lombok

--- src/main/java/com/ada/dto/FilmeDto.java
+++ src/main/java/com/ada/dto/FilmeDto.java
@@ -2,18 +2,15 @@ package com.ada.dto;
 
 import com.ada.model.Filme;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class FilmeDto {
 
 	private String nome;
 	private String descricao;
-
-	private FilmeDto(String nome, String descricao) {
-		this.nome = nome;
-		this.descricao = descricao;
-	}
 
 	public Filme convertToObject() {
 		return Filme.builder()
