4c3dca3ce07c38c35f4f72ce0afbb5028e3aaeaf
André Igor <andreigormenezes@gmail.com>
2022-12-02 11:02:35 -0300

    Fixes

---
+++ src/main/java/com/ada/response/Field.java
@@ -0,0 +1,12 @@
+package com.ada.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Field {
+
+    private String field;
+    private String message;
+}
